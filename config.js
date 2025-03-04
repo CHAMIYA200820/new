7a66e02b2fa50ff914ee129f08c560cd9a3183d7
CHAMIYA200820 <chamindumax33333@gmail.com>
2025-03-04 22:30:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "PINK-QUEEN-MD-j7hDjIQQ#SYLBss6qo4Hwu8fkim0hnAR8Tqr2pxS8kXOu9YoA-D0",
+  SESSION_ID: process.env.SESSION_ID || "PINK-QUEEN-MD-TzxAVBzA#X10vqcFzw4endOIqnyHnizICqlGlDRfVWMqE7PVPJHM",
   MONGODB: process.env.MONGODB || "mongodb://mongo:IEcVaBGSwZNfRMzHDshqhTpKayzYAjwH@shortline.proxy.rlwy.net:43054",
   OWNER_NUM: process.env.OWNER_NUM || "94769040256",
 };
